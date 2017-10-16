a0795f6746d5e7f2a9c48817e115b1a2ad23bb04
Meshiest <meek.mesh@gmail.com>
2017-10-16 18:37:10 -0400

    Added actual course prompt and assignments get request

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const Canvas = require('node-canvas-lms');
 const readYaml = require('read-yaml');
 const leftPad = require('left-pad');
 const _ = require('lodash');
+let prompt = require('prompt');
 
 let course;
 
@@ -26,7 +27,7 @@ readYaml('config.yml', function(err, data) {
   canvas.get('courses', {}, (err, resp, body) => {
     if (err) {
       if(err.statusCode == 401)
-        console.err('Unauthorized config.yml token')
+        console.err('Unauthorized config.yml token');
       else
         throw err;
 
@@ -34,11 +35,34 @@ readYaml('config.yml', function(err, data) {
     }
 
     //console.log(body.map(c => c.enrollments));
-    let numPadding = (body.length + 1 +'').length + 2;
+    let numPadding = (body.length + 1 +'').length + 1;
     console.log('Select a Course: (* means Ta)');
-    _.each(_.sortBy(body, ['type']), (course, i) => {
+    _.each(body, (course, i) => {
       let grading = _.find(course.enrollments, {type: 'ta'}) ? ' *' : '  ';
-      console.log(grading + leftPad(i + 1, numPadding) + ') ' + course.name + ' - ' + course.course_code);
+      console.log(`${grading}${leftPad(i + 1, numPadding)}) ${course.name} - ${course.course_code}`);
+    });
+
+    prompt.start();
+    prompt.message = 'Select';
+    prompt.delimeter = ':';
+
+    prompt.get([{
+      name: 'choice',
+      validator: /^\d+$/,
+      required: true,
+      warning: 'Selection must be numeric',
+    }], (err, result) => {
+      if (err) throw err;
+      let choice = parseInt(result.choice) - 1;
+      if(choice > body.length)
+        console.err('Invalid course selection');
+      else {
+        canvas.get(`courses/${body[choice].id}/assignment`, {}, (err, resp, body) => {
+          if(err) throw err;
+
+          console.log(resp, body);
+        });
+      }
     });
 
   })
