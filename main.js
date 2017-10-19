cb5a26405383e84782f678c1efa9b5c0882cdb0b
Meshiest <meek.mesh@gmail.com>
2017-10-19 18:13:43 -0400

    Started async

--- main.js
+++ main.js
@@ -3,11 +3,28 @@ const Canvas = require('node-canvas-lms');
 const readYaml = require('read-yaml');
 const leftPad = require('left-pad');
 const _ = require('lodash');
+
 let prompt = require('prompt');
+prompt.message = 'Select';
+prompt.delimeter = ':';
 
-let course;
+let canvas;
+const PADDING = 5;
 
-readYaml('config.yml', (err, data) => {
+// Gets a page from canvas
+function get(route, params) {
+  return new Promise((resolve, reject) =>
+    canvas.get(route, _.merge({per_page: 100}, params || {}), (err, resp, body) =>
+      err ? reject(err) : resolve(body)));
+}
+
+// Prompts the user with the given options
+function choose(options) {
+  return new Promise((resolve, reject) => 
+    prompt.get(options, (err, result) => err ? reject(err) : resolve(result)));
+}
+
+readYaml('config.yml', async (err, data) => {
   if (err) {
     console.err('Missing config.yml file');
     return;
@@ -25,83 +42,82 @@ readYaml('config.yml', (err, data) => {
 
   canvas = new Canvas(data.host, data);
   console.log('Fetching Courses...');
-  canvas.get('courses', {}, (err, resp, body) => {
-    if (err) {
-      if(err.statusCode == 401)
-        console.err('Unauthorized config.yml token');
-      else
-        throw err;
+  let courses;
 
+  try {
+    courses = await get('courses');
+  } catch (err) {
+    if(err.statusCode == 401) {
+      console.err('Unauthorized config.yml token');
       return;
-    }
+    } else
+      throw err;
+  }
 
-    //console.log(body.map(c => c.enrollments));
-    let numPadding = (body.length + 1 +'').length + 1;
-    console.log('Select a Course: (* means Ta)');
-    _.each(body, (course, i) => {
-      let grading = _.find(course.enrollments, {type: 'ta'}) ? ' *' : '  ';
-      console.log(`${grading}${leftPad(i + 1, numPadding)}) ${course.name} - ${course.course_code}`);
-    });
+  console.log('Select a Course: (* means Ta)');
+  _.each(courses, (course, i) => {
+    let grading = _.find(course.enrollments, {type: 'ta'}) ? ' *' : '  ';
+    console.log(`${grading}${leftPad(i + 1, PADDING)}) ${course.name} - ${course.course_code}`);
+  });
 
+
+  let choice, result;
+  do {
+    if(choice)
+      console.err('Invalid course selection');
     prompt.start();
-    prompt.message = 'Select';
-    prompt.delimeter = ':';
-
-    prompt.get([{
+    result = choose([{
       name: 'choice',
       validator: /^\d+$/,
       required: true,
       warning: 'Selection must be numeric',
-    }], (err, result) => {
-      if (err) throw err;
-      let choice = parseInt(result.choice) - 1;
-      if(choice > body.length)
-        console.err('Invalid course selection');
-      else {
-        let course = body[choice].id;
+    }]);
+    choice = parseInt(result.choice) - 1;
+  } while(choice > courses.length);
 
-        console.log('Fetching Assignments...');
-        canvas.get(`courses/${course}/assignments`, {}, (err, resp, body) => {
-          numPadding = (body.length + 1 +'').length + 3;
-          console.log('Select an Assignment: ');
-          _.each(body, (assignment, i) =>
-            console.log(`${leftPad(i + 1, numPadding)}) ${assignment.name}`));
+  let course = courses[choice].id;
 
-          if(err) throw err;
-          prompt.start();
-          // console.log(resp, body);
-          prompt.get([{
-            name: 'choice',
-            validator: /^\d+$/,
-            required: true,
-            warning: 'Selection must be numeric',
-          }], (err, result) => {
+  console.log('Fetching Students...', course);
+  let studentPromise = get(`courses/${course}/students`);
 
-            if (err) throw err;
-            let choice = parseInt(result.choice) - 1;
-            if(choice > body.length)
-              console.err('Invalid assignment selection');
-            else {
-              let assignment = body[choice].id;
-              
-              console.log('Fetching Submissions...');
-              canvas.get(`courses/${course}/assignments/${assignment}/submissions`, {per_page: 100}, (err, resp, body) => {
-                numPadding = (body.length + 1 +'').length + 3;
-                //console.log(body);
-                _.each(body, (submission, i) => {
-                  console.log(leftPad(i + 1, numPadding) + ') ' +
-                    (submission.workflow_state === 'submitted' ? '[x]' : '[ ]') + ' ' +
-                    (typeof submission.attachments !== 'undefined' ?
-                      submission.attachments.map(f => f.display_name).join(', ') :
-                      '-'));
-                });
-              });
+  let students = await studentPromise;
+  console.log('Students: ', students);
 
-            }
+  console.log('Fetching Assignments...');
+  let assignments = await get(`courses/${course}/assignments`)
 
-          });
-        });
-      }
-    });
-  })
+  console.log('Select an Assignment: ');
+  _.each(assignments, (assignment, i) =>
+    console.log(`${leftPad(i + 1, PADDING)}) ${assignment.name}`));
+
+
+
+  choice = 0;
+  do {
+    if(choice)
+      console.err('Invalid assignment selection');
+    prompt.start();
+    result = choose([{
+      name: 'choice',
+      validator: /^\d+$/,
+      required: true,
+      warning: 'Selection must be numeric',
+    }]);
+    choice = parseInt(result.choice) - 1;
+  } while(choice > courses.length);
+
+  let assignment = assignments[result.choice].id;
+
+  console.log('Fetching Submissions...');
+  let submissions = await get(`courses/${course}/assignments/${assignment}/submissions`)
+  
+  console.log(assignments);
+  _.each(assignments, (submission, i) => {
+    console.log(leftPad(i + 1, numPadding) + ') ' +
+      (submission.workflow_state === 'submitted' ? '[x]' : '[ ]') + ' ' +
+      (typeof submission.attachments !== 'undefined' ?
+        submission.attachments.map(f => f.display_name).join(', ') :
+        '-'));
+  });
+
 });
